7cef79bb82488ccc26e58ea84073abc96a645725
Sid Raval <sid@thoughtbot.com>
2017-08-04 15:45:44 -0400

    Use `HeadersInit.make` instead of `HeadersInit.makeWithArray`
    https://github.com/reasonml-community/bs-fetch/issues/7

--- src/demo.ml
+++ src/demo.ml
@@ -1,6 +1,6 @@
 let main () = Chrome.Identity.getAuthToken (Chrome.Identity.mkAuthOptions ~interactive:Js.true_ ()) (fun token ->
     Google_drive.getDirectories token
     |> Js.Promise.then_ (fun name -> Js.log name |> Js.Promise.resolve)
-    |> (fun _ -> ()))
+    |> Js.Promise.catch (fun error -> Js.log error |> Js.Promise.resolve))
 
-   let () = Window.onload Window.window main
+let () = Window.onload Window.window main
--- src/google_drive.ml
+++ src/google_drive.ml
@@ -26,7 +26,7 @@ end
 
 let gDriveURL = "https://www.googleapis.com/drive/v3/files?pageSize=1000&q="
                 ^ Window.encodeURIComponent "mimeType = 'application/vnd.google-apps.folder'"
-let authHeaders token = Array.make 1 ("Authorizaton", "Bearer " ^ token) |> HeadersInit.makeWithArray
+let authHeaders token = [%bs.obj { authorization = "Bearer " ^ token }] |> HeadersInit.make
 
 
 let getDirectories token = Js.Promise.(
