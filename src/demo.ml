26d977c124c44e1ae7a1180a5faa25ed1bf0bb92
Sid Raval <sid@thoughtbot.com>
2017-08-05 14:38:12 -0400

    Depend on bs-webapi for `document` and `window` methods

--- src/demo.ml
+++ src/demo.ml
@@ -1,6 +1,14 @@
+open Bs_webapi.Dom
+
+let createFolderDom f = f.Google_drive.files
+                        |> Array.map (fun folder ->
+                            document
+                            |> Document.createElement "div")
+
 let main () = Chrome.Identity.getAuthToken (Chrome.Identity.mkAuthOptions ~interactive:Js.true_ ()) (fun token ->
     Google_drive.getDirectories token
-    |> Js.Promise.then_ (fun name -> Js.log name |> Js.Promise.resolve)
+    |> Js.Promise.then_ (fun name -> createFolderDom name |> Js.Promise.resolve)
+    |> Js.Promise.then_ (fun _ -> () |> Js.Promise.resolve)
     |> Js.Promise.catch (fun error -> Js.log error |> Js.Promise.resolve))
 
-let () = Window.onload Window.window main
+let () = Window.setOnLoad window main
--- src/window.ml
+++
@@ -1,4 +0,0 @@
-type window
-external window : window = "window" [@@bs.val]
-external onload : window -> (unit -> unit) -> unit = "onload" [@@bs.set]
-external encodeURIComponent : string -> string = "encodeURIComponent" [@@bs.val]
